d24135e40c799b92265002ce9e228ba8eeadb367
DanNovik <danil.danila.novikov@mail.ru>
2023-04-15 20:03:13 +0700

    проверка треугольника тесты

---
+++ py_test/DZ16-04Triangel.py
@@ -0,0 +1,40 @@
+import pytest
+def is_valid_side(*sides):  # * -плавающее кол-во параметров
+    for side in sides:
+        if side <= 0:
+            return False
+    return True
+
+# проверка существование треугольник
+# если a+b>c (соответственно для остальных)иначе false
+# стороны треугольника должны быть больше 0, иначе ожидаем false
+def isTriangle(a, b, c):
+    if is_valid_side(a, b, c):
+        if a + b > c and a + c > b and b + c > a:
+            return True
+    return False
+def is_valid_side(*sides):
+    for side in sides:
+        if side <= 0:
+            return False
+    return True
+def isTriangle(a, b, c):
+    if is_valid_side(a, b, c):
+        if a + b > c and a + c > b and b + c > a:
+            return True
+    return False
+def test_is_valid_side():
+    result = is_valid_side(5, 5, 5)
+    assert result == True
+def test_is_valid_side():
+    result = is_valid_side(5, 0, 5)
+    assert result == False
+def test_is_valid_side():
+    result = is_valid_side(5, -8, 5)
+    assert result == False
+def test_isTriangle():
+    result = isTriangle(1, 4, 5)
+    assert result == False
+def test_isTriangle():
+    result = isTriangle(f, 4, 5)
+    assert result == True
